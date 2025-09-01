7ce3e7448bf195f11673fd6d73d645acbfbd5a5d
katekiselevich <kiselkath@gmail.com>
2025-09-01 15:52:12 +0200

    init commit

---
+++ src/main/java/com/smart/expense/usersbudgets/UsersbudgetsApplication.java
@@ -0,0 +1,13 @@
+package com.smart.expense.usersbudgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersbudgetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersbudgetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/expense/usersbudgets/UsersbudgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.expense.usersbudgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersbudgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
